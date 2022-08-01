8059181b449d392c1c8d87a46fa69a08a0d416dc
Richard Rembert <info@rembertdesigns.co>
2022-08-01 16:33:52 -0400

    Hello World

--- C#.cs
+++ C#.cs
@@ -1,4 +1,4 @@
-// C# - Object Oriented Programming language - by Beumsk
+// C# - Object Oriented Programming language - by Richard Rembert
 
 
 // file > new project > web web app > basic + MVC
@@ -15,3 +15,53 @@ using System.IO; // File methods like creating, writing, saving
 using System.Speech.Synthesis; // synthesize strings and more
 using Microsoft.VisualStudio.TestTools.UnitTesting; // testing assembly
 using anyClass; // could be any crafted class (Grades ie)
+
+
+// without VS -> in console
+$ dotnet new console
+$ dotnet restore
+$ dotnet run
+
+
+// Hello world
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args) // Main method is MANDATORY for the code to work and must be STATIC
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
+
+
+// Hello world with prompt
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args) // void is used when there is no return in a method
+    {
+      Console.WriteLine("Your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine("Hello " + name);
+    }
+  }
+}
+
+
+// use parameters from command line
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args) // you can populate parameters through properties > debug; parameters are delimited with spaces; that can be tricked with ""
+    {
+      foreach (string parameters in args) 
+      {
+        Console.WriteLine(parameters);
+      }
+    }
+  }
+}
