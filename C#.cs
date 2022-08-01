cef40a350caa8e90424e7740f2a3597630c98ba9
Richard Rembert <info@rembertdesigns.co>
2022-08-01 16:32:28 -0400

    Starting C#

---
+++ C#.cs
@@ -0,0 +1,17 @@
+// C# - Object Oriented Programming language - by Beumsk
+
+
+// file > new project > web web app > basic + MVC
+
+
+// useful starting lines; they are assemblies (libraries) that contain useful objects, methods, functions.
+using System; // contain for example the Console object
+using System.Collections.Generic; // contains the list 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// less usual assembly
+using System.IO; // File methods like creating, writing, saving 
+using System.Speech.Synthesis; // synthesize strings and more
+using Microsoft.VisualStudio.TestTools.UnitTesting; // testing assembly
+using anyClass; // could be any crafted class (Grades ie)
