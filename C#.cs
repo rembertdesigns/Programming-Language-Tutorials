0d95fdf02738f3dcf808be4e893a5ec0675ac769
Richard Rembert <info@rembertdesigns.co>
2022-08-03 16:35:54 -0400

    Type Casting

--- C#.cs
+++ C#.cs
@@ -82,3 +82,14 @@ bool diff; // true or false
 type[] arr; // array of any type (ex int); reference type
 List<type> list; // list of any type (ex int)
 DayOfWeek day; // day of week enum
+
+
+// Type casting
+short s = 200; int i = s; // 200 becomes of int type
+int i = 200; short s = i; // not possible; short could be too specific for the number
+int i = 200; short s = (short)i; // possible because we cast 'explicitly'
+int i = 40000; short s = (short)i; // not possible even with cast 'explicitly'
+int i = 20; double d = i; // 20 becomes of double type
+int age = 24; string ageStr = Convert.ToString(age); // converts int to string "24"
+string ageStr = "24"; int age = int.Parse(ageStr); // converts string to int 24
+int.TryParse(ageStr, out age); // checks if parse is authorized and stores value in out int; will return true here
