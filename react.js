40e761fc9269bec1d8ac558227d8d53e28d5b803
Richard Rembert <info@rembertdesigns.co>
2022-08-21 22:03:36 -0400

    Show how to set up DEV

--- react.js
+++ react.js
@@ -14,3 +14,28 @@
 // blitz -> not very established; good doc but not for migration; fast even with API query; needs a server (except on Vercel)
 // redwood -> not very established; JAM stack; tricky migration; deploy everywhere; meant to be serverless
 // remix -> very recent
+
+
+
+// SETUP DEV
+
+// start a new react project from scratch -> https://jscomplete.com/learn/1rd-reactful
+cd projectFolder
+// create package.json
+npm init -y
+// express to run node server
+npm i express
+// install react & react-dom
+npm i react react-dom
+// install webpack, a module bundler
+npm i webpack webpack-cli
+// install babel
+npm i babel-loader @babel/core @babel/node @babel/preset-env @babel/preset-react
+// dev dependencies
+// nodemon or alternative to change server code without restarting node
+npm i -D nodemon
+// eslint -> add a .eslintrc.json file
+npm i -D eslint babel-eslint eslint-plugin-react eslint-plugin-react-hooks 
+/* eslint-config-prettier eslint-config-airbnb eslint-plugin-cypress eslint-plugin-import eslint-plugin-jest eslint-plugin-jsx-a11y eslint-plugin-prettier */
+// jest for testing
+npm i -D jest babel-jest react-test-renderer
