6811de01be914f21286a33e1d47f153427126133
Richard Rembert <info@rembertdesigns.co>
2022-08-21 22:19:56 -0400

    Components 2

--- react.js
+++ react.js
@@ -594,4 +594,86 @@ class MyComponentClass extends React.Component {
     }
   }
   ReactDOM.render(<Button />, document.getElementById('app'));
+
+
+
+  // COMPONENTS RENDER OTHER COMPONENTS
+
+// Component render another component
+class OMG extends React.Component {
+    render() {
+      return <h1>Whooaa!</h1>;
+    }
+  }
+  class Crazy extends React.Component {
+    render() {
+      return (
+        <div>
+          <OMG />
+          <p>Amazing.</p>
+        </div>
+      );
+    }
+  }
+  
+  
+  // Import a component from another file
+  // exported OMG class in file OMG.js
+  export class OMG extends React.Component {
+    render() {
+      return <h1>Whooaa!</h1>;
+    }
+  }
+  // import OMG class from OMG.js
+  import { OMG } from './OMG.js';
+  class Crazy extends React.Component {
+    render() {
+      return (
+        <div>
+          <OMG />
+          <p>Amazing.</p>
+        </div>
+      );
+    }
+  }
+  
+  // Pay attention to the export method !!! 
+  export default class ComponentName extends React.Component {};
+  import ComponentName from './path';
+  
+  export class ComponentName extends React.Component {};
+  import { ComponentName } from './path';
+  
+  
+  // accurate and full example of imported component
+  // file NavBar.js
+  import React from 'react';
+  export class NavBar extends React.Component {
+    render() {
+      const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+      const navLinks = pages.map(page => {
+        return (
+          <a href={'/' + page}>{page}</a>
+        )
+      });
+      return <nav>{navLinks}</nav>;
+    }
+  }
+  // file ProfilePage.js
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import { NavBar } from './NavBar';
+  class ProfilePage extends React.Component {
+    render() {
+      return (
+        <div>
+          <NavBar />
+          <h1>All About Me!</h1>
+          <p>I like movies and blah blah blah blah blah</p>
+          <img src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyselfie.jpg" />
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(<ProfilePage />, document.getElementById('app'));
   
