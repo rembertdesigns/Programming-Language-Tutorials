0db317962383a0b95c2b220495a4ee944a0fc9ed
Richard Rembert <info@rembertdesigns.co>
2022-08-21 22:01:26 -0400

    Starting React

---
+++ react.js
@@ -0,0 +1,16 @@
+// REACT - Javascript Library - by Richard Rembert
+
+
+
+/**/
+
+
+
+// REACT FRAMEWORKS
+
+// create-react-app -> 1st party; simple and basic; it is opinionated; many dependencies that could be useless
+// next.js -> ssr or ssg; very fast; integrates best with Vercel hosting which costs
+// gatsby -> very fast; plugins and themes available; hard to migrate; official gatsby hosting costs
+// blitz -> not very established; good doc but not for migration; fast even with API query; needs a server (except on Vercel)
+// redwood -> not very established; JAM stack; tricky migration; deploy everywhere; meant to be serverless
+// remix -> very recent
