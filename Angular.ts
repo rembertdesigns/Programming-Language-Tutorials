c6e74c9131cfc7698d6fd87ce2136a2995cb0269
Richard Rembert <info@rembertdesigns.co>
2022-08-25 12:58:07 -0400

    Services

--- Angular.ts
+++ Angular.ts
@@ -77,3 +77,85 @@ import { AppComponent } from './app.component';
 })
 
 export class AppModule {}
+
+
+// SERVICES 
+
+  // in a model ts file
+  export class ModelName {
+    id: number;
+    date: Date;
+    weight: number;
+  }
+
+  // in a .service.ts file
+  import { Injectable } from "@angular/core";
+  import { ModelName } from "./modelName";
+
+  @Injectable({
+    providedIn: 'root???'
+  })
+  export class NameService {
+    public arr: ModelName[] = [
+      {id:1, date:new Date(), weight:75},
+      {id:2, date:new Date(), weight:80},
+      {id:3, date:new Date(), weight:85},
+    ]
+
+    constructor(
+      public namesSvc: NameService
+    ) {}
+  }
+
+
+// INPUT (parent to child)
+
+  // .html (parent)
+  @component({ template: `
+    <child-component [show]="show"></child-component>
+  `})
+
+  // .component.ts (child)
+  import { Component, OnInit, Input } from '@angular/core';
+  @Component({})
+  export class ChildrenComponent implements OnInit {
+    @Input() show: boolean;
+  }
+
+
+// OUTPUT (child to parent)
+
+  // .component.ts (child)
+  import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+  @Component({})
+  export class ChildrenComponent implements OnInit {
+    @Output() create = new EventEmitter();
+    constructor() {}
+    public createSmth(): void {
+      this.create.emit("Any data you want in your parent");
+    }
+  }
+
+  // .html (parent)
+  `<child-component (create)="createSmthElse($event)"></child-component>`
+
+  // .component.ts (parent)
+  public createSmthElse(line: string): void {
+    console.log(line);
+  }
+
+
+// VIEWCHILD (bind HTML elements with Angular elements)
+
+  // .component.ts
+  import { AfterViewInit, Component, ViewChild } from '@angular/core';
+  @Component({})
+  export class ChildrenComponent implements AfterViewInit {
+    @ViewChild('htmlElement') htmlElement: any;
+    ngAfterViewInit() {
+      console.log(this.htmlElement);
+    }
+  }
+
+  // .html
+  `<any-element #htmlElement></any-element>`
