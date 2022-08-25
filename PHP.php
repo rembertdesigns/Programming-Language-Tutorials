ae33631ac6b189dfb2f7871325a7afa277e070d1
Richard Rembert <info@rembertdesigns.co>
2022-08-25 12:38:47 -0400

    Server Scripting

---
+++ PHP.php
@@ -0,0 +1,13 @@
+HYPERTEXT PREPROCESSOR
+  -Server Scripting
+    -by Richard Rembert
+
+
+// To create a comment. Shown in code only. Indent 2spaces
+/* Multiple line comment */
+
+<?php ?> or <? ?> // PHP tags
+
+<?php echo "texte qui sera écrit"; ?>
+<?php print "texte qui sera écrit"; ?>
+<?php include("fichier.php"); ?>
