de14f6831b5e271d22b7967db85959017e187228
Richard Rembert <info@rembertdesigns.co>
2022-08-25 12:53:05 -0400

    Angular Help

---
+++ Angular.ts
@@ -0,0 +1,25 @@
+// ANGULAR - Javascript Framework - by Richard Rembert
+
+
+
+/**/
+// npm install @angular/cli -g
+// npm i @angular/cli -g
+// ng new <projectName>
+// npm install
+// npm install tslint
+// npm install --save json-server
+// ng generate component <componentName>
+// ng g c <componentName>
+// ng g c <folder>/<componentName>
+// ng g service <serviceName>
+// ng serve
+// npm start
+// npm run <custom-script from package.json>
+// ng build
+// ng build --prod
+// prod files are now in dist/<projectName>
+// more cli commands -> https://malcoded.com/posts/angular-fundamentals-cli/
+
+
+// Angular cheat sheet -> https://angular.io/guide/cheatsheet
--- PHP.php
+++ PHP.php
@@ -8,6 +8,32 @@ HYPERTEXT PREPROCESSOR
 
 <?php ?> or <? ?> // PHP tags
 
-<?php echo "texte qui sera écrit"; ?>
-<?php print "texte qui sera écrit"; ?>
-<?php include("fichier.php"); ?>
+<?php echo "text that will be written"; ?>
+<?php print "text that will be written"; ?>
+<?php include("file.php"); ?>
+
+
+// VARIABLES
+
+
+
+<?php $name_of_visitor = "Richard"; ?> // string = "text"
+<?php $age_du_visitor = 22; ?> // int = 42
+<?php $visitor_weight = 73.5; ?> // float = 42.98
+<?php $je_suis_un_zero = true; ?> // bool = true/false
+<?php $no_value = null; ?> // NULL = nothing
+
+
+
+<?php echo 'Visitor has '. $age_of_visitor . ' year.'; ?>
+
+
+
+<?php $number = (3+4-2)*8/5; ?> // number is 8
+<?php
+$number = 10; // number is 10
+$result = $number+10; // result is 20
+?>
+<?php $number = 10%3; ?> //number is 1 pcq modulo (%) calculates the remainder of the division
+
+
