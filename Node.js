dd7dcd20d29775f994c7926ef73ce5b10b4aefb2
Richard Rembert <105387636+rembertdesigns@users.noreply.github.com>
2023-06-07 09:29:30 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -80,3 +80,43 @@ app.put('/route', function() {});
 
 // chain methods with this syntax
 app.route('/route').get(function() {}).post(function() {});
+
+
+// ??ACTIONS??
+app.get('/', function(req, res) {
+  res.send('Hello World');
+  res.sendFile(__dirname + '/index.html'); // needs absolute path
+});
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/json', function(req, res) {
+  res.json({"message": "Hello json"});
+  const method = req.method; // GET, POST, DELETE, PUT
+  const path = req.path; // for example /json
+  const ip = req.ip; // ip address
+});
+app.get('/user/:userId', function(req, res) { // /user/543
+  const params = req.params; // route parameters {userId: '543'}
+  res.json({'userId': params.userId});
+});
+app.get('/user', function(req, res) { // /user?userId=543
+  const query = req.query; // route query {userId: '543'}
+  res.json({'userId': query.userId});
+});
+
+
+
+
+
+
+
+
+// ------------------------ MONGODB ------------------------ //
+// https://www.freecodecamp.org/news/get-started-with-mongodb-atlas/
+// https://www.freecodecamp.org/news/introduction-to-mongoose-for-mongodb-d2a7aa593c57/
+// New project 
+// new DB (or cluster) 
+// new user with 'read and write to any database' (security > database access)
+// access all API (security > network access)
+// connect (deployment > database) => mongodb+srv://<username>:<password>@<cluster-name>.prx1c.mongodb.net/<db-name>?retryWrites=true&w=majority
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://<username>:<password>@<cluster-name>.fnwaqdz.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
