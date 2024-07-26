8878634be14c0ee8cf59d0b6858003e946a38977
Richard Rembert <105387636+rembertdesigns@users.noreply.github.com>
2024-07-26 12:52:21 -0400

    Fundamental Data Types

--- main.py
+++ main.py
@@ -2,5 +2,34 @@
 # Comments start with # & will show code below with examples
 
 # First Python script people use 
-
 print("Hello, World!") 
+
+
+# Primitive (Fundamental) Data Types:
+# Primitive data types represent simple values. These data types are the most basic and essential units used to store and manipulate information in a program. They translate directly into low-level machine code.
+
+# Primitive data types include:
+String (str): Represents sequences of characters. Should be enclosed in quotes. Example: "Hello, Python!"
+Integer (int): Represents whole numbers without decimals. Example: 42
+Float (float): Represents numbers with decimals. Example: 3.14
+Boolean (bool): Represents either True or False.
+
+# String Example 
+name = "John"
+# Integer Example
+age = 25 
+# Float Example 
+height = 1.75 
+# Boolean Example
+is_student = True
+# Print variable values 
+print("Name:", name) 
+print("Age:", age) 
+print("Height:", height)
+print("Is student?", is_student)
+
+#Output
+Name: John
+Age: 25
+Height: 1.75
+Is student? True
